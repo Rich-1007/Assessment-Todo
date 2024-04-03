dbe56163a427cc4737e9a2631cd9b77e1dc043ea
Rich-1007 <ghricheak@gmail.com>
2024-04-04 00:40:21 +0530

    updates

--- src/Components/AddTodoForm.jsx
+++ src/Components/AddTodoForm.jsx
@@ -14,44 +14,7 @@ const AddTodoForm = () => {
     setDescription("")
   };
   return (
-    <div>
-      <form onSubmit={submitToDoHandler}>
-        <div className="py-1">
-          <span className="hover:border-b-2 border-blue-500">Task Title</span>
-        </div>
-
-        <div className="pb-4">
-          <input
-            value={title}
-            type="text"
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Enter task title"
-            className="p-1.5 rounded-md border-2 hover:border-purple-500  "
-          />
-        </div>
-
-        <div className="py-1">
-          <span className="hover:border-b-2 border-blue-500">
-            Task Description
-          </span>
-        </div>
-
-        <div className="pb-4">
-          <input
-            value={description}
-            type="text"
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder="Enter task description"
-            className=" p-1.5 pb-8 rounded-md border-2 hover:border-blue-500"
-          />
-        </div>
-
-        <div className="bg-blue-900 text-white flex justify-center rounded-md p-1  border-2 hover:border-purple-500">
-          <button>Add Task</button>
-        </div>
-
-      </form>
-    </div>
+    <div></div>
   );
 };
 
